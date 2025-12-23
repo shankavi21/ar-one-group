a6bcd0f5a9d423ea94bbd7533764fab68ec7c30d
shankavi21 <kavihana884@gmail.com>
2025-12-23 14:03:50 +0530

    change db

--- src/utils/guideAuth.js
+++ src/utils/guideAuth.js
@@ -3,12 +3,12 @@ import { getAuth, createUserWithEmailAndPassword, signOut } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBHEfd7QA8gvcgiwO3coXDwiE4DILUGXz4",
-    authDomain: "ar-one-33f7d.firebaseapp.com",
-    projectId: "ar-one-33f7d",
-    storageBucket: "ar-one-33f7d.firebasestorage.app",
-    messagingSenderId: "985849759746",
-    appId: "1:985849759746:web:06753bccee0a3d8c25ff1b"
+    apiKey: "AIzaSyCAFYZ6tXphRrT9hOI1feDfPJlc8yJxLyI",
+    authDomain: "ar0ne-a1675.firebaseapp.com",
+    projectId: "ar0ne-a1675",
+    storageBucket: "ar0ne-a1675.firebasestorage.app",
+    messagingSenderId: "648362500714",
+    appId: "1:648362500714:web:cc12ca6d27a1755ceda013"
 };
 
 // Initialize a secondary Firebase app for creating guide accounts
